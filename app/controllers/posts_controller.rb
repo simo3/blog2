902ee5a7fa33df8bc330010da7a3ff2251850331
下向和紀 <simo@simo3.local>
2015-09-14 22:32:02 +0900

    controllerに着手

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,28 @@
 class PostsController < ApplicationController
+  def index
+  end
+
+  def create
+
+  end
+
+  def new
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end
