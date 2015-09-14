d3a54a2487e23b3b68c24e7efb0120d88c1656ea
下向和紀 <simo@simo3.local>
2015-09-14 22:59:26 +0900

    new.html.erb show.html.erb作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,15 +3,17 @@ class PostsController < ApplicationController
   end
 
   def create
-
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to @post
   end
 
   def new
-
+    @post = Post.new
   end
 
   def show
-
+    @post = Post.find(params[:id])
   end
 
   def edit
@@ -24,5 +26,13 @@ class PostsController < ApplicationController
 
   def destroy
 
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(
+      :title,
+      :content,
+      :category)
   end
 end
