d5eeee3baebfe5507aca45a1317f7c5872e46465
下向和紀 <simo@simo3.local>
2015-09-18 14:37:21 +0900

    ユーザー情報　姓名追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150915133804) do
+ActiveRecord::Schema.define(version: 20150918052539) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 20150915133804) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "family_name"
+    t.string   "given_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
